41298abcf629611452c1ce93d15be690d42dd5ff
zhangkai <1310702079@qq.com>
2016-12-06 16:25:06 +0800

    修改工具类

--- src/main/java/Utils/FileUtils.java
+++ src/main/java/Utils/FileUtils.java
@@ -7,20 +7,31 @@ import java.util.List;
 /**
  * Created by ZK on 2016/11/28.
  */
-public class DirTraversal {
+public class FileUtils {
 
     //递归查找指定目录下的所有指定拓展名的文件，将返回文件名的list
-    public static List<String> findXml(String suffix,String dirName) {
+    public static List<String> findByEx(String suffix,String dirName) {
         File file = new File(dirName);
         File[] files = file.listFiles();
         List result = new ArrayList();
         for (File f : files) {
             if (f.isDirectory()) {
-                result.addAll(findXml(suffix,f.getPath()));
+                result.addAll(findByEx(suffix,f.getPath()));
             } else if (f.getName().endsWith("." + suffix)) {
                 result.add(f.getPath());
             }
         }
         return result;
+    }
+
+    //返回无扩展名的文件名称
+    public static String getFileNameNoEx(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot >-1) && (dot < (filename.length()))) {
+                return filename.substring(0, dot);
+            }
+        }
+        return filename;
     }
 }
